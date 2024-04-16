06b66b9fb27a578adf74ceec1dee19639b7d8738
tnsgud <qkrtnsgud0229@gmail.com>
2024-04-17 00:51:07 +0900

    feat: `드론관련 콘텐츠` 위젯 구현 [지라 이슈 링크 또는 관련 이슈 번호]
    사이즈를 고정해서 디자인 구현함
    다르 해상도에서 어떻게 사이즈를 지정할지
    고민해야함

--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -12,14 +12,77 @@ class HomeFragment extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            header(),
+            banner(),
+            popularPilots(),
+            useCaseGuide(),
+            droneContent()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding droneContent() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 24),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          header(),
-          banner(),
-          popularPilots(),
-          useCaseGuide(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  '드론관련 콘텐츠',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SvgPicture.asset('assets/image/icon/chevron_right.svg')
+              ],
+            ),
+          ),
+          const Height(12),
+          SizedBox(
+            height: 186,
+            child: ListView.separated(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => SizedBox(
+                width: 132,
+                child: Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 132,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    const Height(12),
+                    Text(
+                      '드론콘텐츠 제목은 2줄까지 노출합니다!',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey[600],
+                      ),
+                      overflow: TextOverflow.clip,
+                    ),
+                  ],
+                ),
+              ),
+              separatorBuilder: (context, index) => const Width(8),
+              itemCount: 3,
+            ),
+          )
         ],
       ),
     );
