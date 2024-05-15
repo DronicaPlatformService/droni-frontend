36cb9c61998fc61a90d0b157e1179491841a39c1
tnsgud <60930113+tnsgud@users.noreply.github.com>
2024-05-15 21:16:46 +0900

    style: 텍스트 스타일 추가

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 export 'dart:async';
@@ -25,5 +26,24 @@ export 'widget/w_line.dart';
 export 'widget/w_tap.dart';
 
 const defaultFontStyle = GoogleFonts.ptSerif;
+
+const system01 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 20, height: 28 / 20);
+const system02 = TextStyle(fontSize: 20, height: 28 / 20);
+const system03 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 18, height: 26 / 18);
+const system04 = TextStyle(fontSize: 18, height: 26 / 18);
+const system05 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 16, height: 24 / 16);
+const system06 = TextStyle(fontSize: 16, height: 24 / 16);
+const system07 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 14, height: 20 / 14);
+const system08 = TextStyle(fontSize: 14, height: 20 / 14);
+const system09 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 12, height: 18 / 12);
+const system10 = TextStyle(fontSize: 12, height: 18 / 12);
+const system11 =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 10, height: 14 / 10);
+const system12 = TextStyle(fontSize: 10, height: 14 / 10);
 
 void voidFunction() {}
