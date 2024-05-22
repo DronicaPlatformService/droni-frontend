dcc1116c4bc8f56187b6b5edab1b88671618906c
tnsgud <60930113+tnsgud@users.noreply.github.com>
2024-05-22 15:45:42 +0900

    🍱 Add fonts
    DRONI-107

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -11,21 +11,61 @@ export 'theme/shadows/abs_theme_shadows.dart';
 
 const defaultFontStyle = GoogleFonts.ptSerif;
 
-const system01 =
-    TextStyle(fontWeight: FontWeight.bold, fontSize: 20, height: 28 / 20);
-const system02 = TextStyle(fontSize: 20, height: 28 / 20);
-const system03 =
-    TextStyle(fontWeight: FontWeight.bold, fontSize: 18, height: 26 / 18);
-const system04 = TextStyle(fontSize: 18, height: 26 / 18);
-const system05 =
-    TextStyle(fontWeight: FontWeight.bold, fontSize: 16, height: 24 / 16);
-const system06 = TextStyle(fontSize: 16, height: 24 / 16);
-const system07 =
-    TextStyle(fontWeight: FontWeight.bold, fontSize: 14, height: 20 / 14);
-const system08 = TextStyle(fontSize: 14, height: 20 / 14);
-const system09 =
-    TextStyle(fontWeight: FontWeight.bold, fontSize: 12, height: 18 / 12);
-const system10 = TextStyle(fontSize: 12, height: 18 / 12);
+const system01 = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+  height: 28 / 20,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system02 = TextStyle(
+  fontSize: 20,
+  height: 28 / 20,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system03 = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+  height: 26 / 18,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system04 = TextStyle(
+  fontSize: 18,
+  height: 26 / 18,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system05 = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+  height: 24 / 16,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system06 = TextStyle(
+  fontSize: 16,
+  height: 24 / 16,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system07 = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+  height: 20 / 14,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system08 = TextStyle(
+  fontSize: 14,
+  height: 20 / 14,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system09 = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 12,
+  height: 18 / 12,
+  fontFamily: 'SpoqaHanSansNeo',
+);
+const system10 = TextStyle(
+  fontSize: 12,
+  height: 18 / 12,
+  fontFamily: 'SpoqaHanSansNeo',
+);
 const system11 =
     TextStyle(fontWeight: FontWeight.bold, fontSize: 10, height: 14 / 10);
 const system12 = TextStyle(fontSize: 10, height: 14 / 10);
