27f0ff4847d4a101966512ebedf1a3896fbbeafd
tnsgud <60930113+tnsgud@users.noreply.github.com>
2024-05-23 11:24:34 +0900

    💄 Add styles files

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // DRONI COLOR BLUE
+  static const Color droniBlue100 = Color.fromARGB(255, 208, 223, 255);
+  static const Color droniBlue200 = Color.fromARGB(255, 162, 191, 255);
+  static const Color droniBlue300 = Color.fromARGB(255, 115, 160, 255);
+  static const Color droniBlue400 = Color.fromARGB(255, 69, 128, 255);
+  static const Color droniBlue500 = Color.fromARGB(255, 22, 96, 255);
+  static const Color droniBlue600 = Color.fromARGB(255, 18, 77, 204);
+  static const Color droniBlue700 = Color.fromARGB(255, 13, 58, 153);
+  static const Color droniBlue800 = Color.fromARGB(255, 9, 38, 102);
+  static const Color droniBlue900 = Color.fromARGB(255, 4, 19, 51);
+
+  // DRONI COLOR GRAY
+  static const Color droniGray100 = Color.fromARGB(255, 247, 248, 250);
+  static const Color droniGray200 = Color.fromARGB(255, 235, 237, 240);
+  static const Color droniGray300 = Color.fromARGB(255, 206, 208, 214);
+  static const Color droniGray400 = Color.fromARGB(255, 158, 161, 172);
+  static const Color droniGray500 = Color.fromARGB(255, 109, 113, 131);
+  static const Color droniGray600 = Color.fromARGB(255, 61, 66, 89);
+  static const Color droniGray700 = Color.fromARGB(255, 45, 50, 71);
+  static const Color droniGray800 = Color.fromARGB(255, 32, 35, 50);
+  static const Color droniGray900 = Color.fromARGB(255, 2, 4, 10);
+}
---
+++ lib/constants/svg_icon.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SvgIcon extends StatelessWidget {
+  const SvgIcon(
+      {super.key, required this.icon, this.width, this.height, this.color});
+
+  final String icon;
+  final double? width;
+  final double? height;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      icon,
+      width: width,
+      height: height,
+      colorFilter:
+          color != null ? ColorFilter.mode(color!, BlendMode.srcIn) : null,
+    );
+  }
+}
