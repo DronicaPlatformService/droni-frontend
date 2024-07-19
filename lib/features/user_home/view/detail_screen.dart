e8da35d9953f91b06190f0eff4fec06364fc5654
tnsgud <60930113+tnsgud@users.noreply.github.com>
2024-07-19 22:00:35 +0900

    🚩 Connect content screen

--- lib/features/user_home/view/detail_screen.dart
+++ lib/features/user_home/view/detail_screen.dart
@@ -1,11 +1,15 @@
 import 'dart:math';
 
-import 'package:droni/api/generated/openAPI.models.swagger.dart';
-import 'package:droni/features/user_home/view/widgets/drone_content.dart';
-import 'package:droni/features/user_home/view/widgets/use_case_guide_item.dart';
+import 'package:flutter/material.dart';
+
+import 'package:droni/shared/widgets/svg_icon.dart';
 import 'package:droni/shared/constants/app_colors.dart';
 import 'package:droni/shared/constants/text_style.dart';
-import 'package:flutter/material.dart';
+
+import 'package:droni/api/generated/openAPI.models.swagger.dart';
+
+import './content_screen.dart';
+import './widgets/content_card.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key});
@@ -14,13 +18,26 @@ class DetailScreen extends StatelessWidget {
     Navigator.of(context).pop();
   }
 
+  void _onContentTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ContentScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: GestureDetector(
           onTap: () => _onBackTap(context),
-          child: const Icon(Icons.arrow_back_ios_new_rounded),
+          child: const Padding(
+            padding: EdgeInsets.all(14.0),
+            child: SvgIcon(
+              icon: 'assets/image/icon/chevron_left.svg',
+            ),
+          ),
         ),
         title: Text(
           '타이틀',
@@ -29,21 +46,24 @@ class DetailScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.white),
-        child: GridView.builder(
-          padding: const EdgeInsets.all(20),
-          itemCount: 10,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 1 / 1.14,
-          ),
-          itemBuilder: (context, index) => UseCaseGuideItem(
-            subTitle: index % 2 == 0 ? '일반사용자' : '조종사',
-            article: ArticleSummaryResponse(
-              articleImageUri:
-                  'https://picsum.photos/id/${Random().nextInt(20)}/160/120',
-              articleSubject: 'test',
+      body: GestureDetector(
+        onTap: () => _onContentTap(context),
+        child: Container(
+          decoration: const BoxDecoration(color: Colors.white),
+          child: GridView.builder(
+            padding: const EdgeInsets.all(20),
+            itemCount: 10,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 1 / 1.14,
+            ),
+            itemBuilder: (context, index) => ContentCard(
+              subTitle: index % 2 == 0 ? '일반사용자' : '조종사',
+              article: ArticleSummaryResponse(
+                articleImageUri:
+                    'https://picsum.photos/id/${Random().nextInt(20)}/160/120',
+                articleSubject: 'test',
+              ),
             ),
           ),
         ),
