de6826b84b923e1801eb6cb64b4849ea0b96a45e
tnsgud <60930113+tnsgud@users.noreply.github.com>
2024-07-19 22:01:19 +0900

    🚩 Connect detail screen

--- lib/features/user_home/view/widgets/drone_content.dart
+++ lib/features/user_home/view/widgets/drone_content.dart
@@ -1,12 +1,24 @@
-import 'package:droni/shared/constants/app_colors.dart';
-import 'package:droni/shared/constants/text_style.dart';
+import 'package:flutter/material.dart';
+
+import 'package:gap/gap.dart';
+
 import 'package:droni/shared/utils/api_client.dart';
 import 'package:droni/shared/widgets/svg_icon.dart';
-import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
+import 'package:droni/shared/constants/text_style.dart';
+
+import './content_card.dart';
+import '../../view/detail_screen.dart';
 
 class DroneContent extends StatelessWidget {
   const DroneContent({super.key});
+
+  void _onDetailTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const DetailScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +26,18 @@ class DroneContent extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 24),
       child: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('드론관련 콘텐츠', style: system03),
-                SvgIcon(icon: 'assets/image/icon/chevron_right.svg')
+                const Text('드론관련 콘텐츠', style: system03),
+                GestureDetector(
+                  onTap: () => _onDetailTap(context),
+                  child: const SvgIcon(
+                    icon: 'assets/image/icon/chevron_right.svg',
+                  ),
+                )
               ],
             ),
           ),
@@ -46,37 +63,9 @@ class DroneContent extends StatelessWidget {
                 return ListView.separated(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
                   scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) => SizedBox(
+                  itemBuilder: (context, index) => ContentCard(
                     width: 132,
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 132,
-                          clipBehavior: Clip.hardEdge,
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Image.network(
-                            data[index].articleImageUri!,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        const Gap(12),
-                        SizedBox(
-                          width: double.infinity,
-                          child: Text(
-                            // '드론콘텐츠 제목은 2줄까지 노출합니다!',
-                            data[index].articleSubject ?? 'error',
-                            textAlign: TextAlign.start,
-                            style: system08.copyWith(
-                              color: AppColors.droniGray600,
-                            ),
-                            overflow: TextOverflow.clip,
-                          ),
-                        ),
-                      ],
-                    ),
+                    article: data[index],
                   ),
                   separatorBuilder: (context, index) => const Gap(8),
                   itemCount: 3,
